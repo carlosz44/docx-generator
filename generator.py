90f51887ab2629629a10b8edd629876c3e4a3d54
Carlos Amoros <camorost@gmail.com>
2023-01-17 22:45:24 -0500

    updated script

--- generator.py
+++ generator.py
@@ -1,23 +1,16 @@
 from docx import Document
 import pandas as pd
 
-df = pd.read_excel('data.xlsx')
-data = df.to_dict('records')
+dataFile = pd.read_excel('data.xlsx')
+data = dataFile.to_dict('records')
 
 for item in data:
     template = Document('template.docx')
     item['nameCap'] = item['name'].upper()
+    item['document'] = str(item['document'])
 
     for para in template.paragraphs:
-        if '{name}' in para.text:
-            para.text = para.text.replace('{name}', item['name'])
-        if '{nameCap}' in para.text:
-            para.text = para.text.replace('{nameCap}', item['nameCap'])
-        if '{document}' in para.text:
-            para.text = para.text.replace('{document}', str(item['document']))
-        if '{marital}' in para.text:
-            para.text = para.text.replace('{marital}', item['marital'])
-        if '{address}' in para.text:
-            para.text = para.text.replace('{address}', item['address'])
+        for key, value in item.items():
+            para.text = para.text.replace('{'+key+'}', value)
 
     template.save('output/' + str(item['document']) + '.docx')
